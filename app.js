86d529056e5cc5f6268710a3ec323d7a983d20fc
EmilyJarecki <jareckiemily@yahoo.com>
2022-11-04 18:03:53 -0500

    Commit 5 - Sum Array

--- app.js
+++ app.js
@@ -48,12 +48,19 @@ function getTwoLengths(word1, word2) {
     arr.push(word1.length, word2.length)
     return arr
 }
-console.log(getTwoLengths("Hank", "Hippopopalous"))
+//console.log(getTwoLengths("Hank", "Hippopopalous"))
 
 // 5. sumArray
 function sumArray(arr) {
     // YOUR CODE HERE
+    sum = 0
+    for (let i = 0; i<arr.length; i++){
+        sum += arr[i]
+    }
+    return sum 
 }
+console.log(sumArray([1, 2, 3, 4, 5, 6]))
+
 
 // 6.1 checkPrime
 function checkPrime(num) {
